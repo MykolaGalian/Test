4a5a8059b04a55f9559076452698a53261c982c6
MykolaGal <gnnkolya@gmail.com>
2019-07-03 19:50:56 +0300

    Update Program.cs

--- Task2_Ar/Task2_Ar/Program.cs
+++ Task2_Ar/Task2_Ar/Program.cs
@@ -24,63 +24,7 @@ namespace Task2_Ar
         }
     }
 
-
-    class Program
-    {
-        
-        static void Main(string[] args)
-        {
-           // int _start = -4; //начальный индекс массива
-           // int _end =2;    //конечный индекс массива -1
-
-           // // создание массива string -ов с заданными границами индексов
-           // var array = new CustomArray<string>(_start, _end);
-           // //заполнение массива
-           //// var rnd = new Random();    // для int
-           // for (int i = _start; i < _end; i++)
-           // {
-           //     array[i] = "ABCDEFGHIJKLMNOP".Random();   //rnd.Next(0, 10);
-           // }
-
-           // Console.WriteLine("Array lenght: " + array.Lenght + "\n");
-
-           // foreach (var n in array) {
-           //     Console.WriteLine($"array element: {n.ToString()}");
-           // }
-           // Console.WriteLine("\n");
-
-           // //Range генерирует последовательность целых чисел - начиная со значения, переданного в первом арг., протяженностью второго арг.
-           // Console.WriteLine(string.Join("\n", Enumerable.Range(_start, (_end - _start)).Select(x => string.Format("index:{0} - value:{1}", x, array[x]))));
-
-
-
-
-            Console.WriteLine();
-            CustomArray<int>  arTests = new CustomArray<int>(-4, 2);
-            for (int i = arTests.Start; i < arTests.End; i++)
-            {
-                arTests[i] = i*2;
-            }
-
-            Console.WriteLine("Array lenght: " + arTests.Lenght + "\n");
-
-            foreach (var n in arTests)
-            {
-                Console.WriteLine($"array element: {n.ToString()}");
-            }
-            Console.WriteLine("\n");
-            //Range генерирует последовательность целых чисел - начиная со значения, переданного в первом арг., протяженностью второго арг.
-            Console.WriteLine(string.Join("\n", Enumerable.Range(arTests.Start, (arTests.End - arTests.Start)).Select(x => string.Format("index:{0} - value:{1}", x, arTests[x]))));
-
-           // CustomArray<int> arTests1 = new CustomArray<int>(-4, -8);
-            Console.ReadKey();
-
-
-
-
-        }
-    }
-
+    
     public class CustomArray<T> : IEnumerable<T>
     {
         private int start;
